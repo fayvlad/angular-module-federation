e2af00be18bc7cb9eb1076b035bd05327b15a71f
Vladyslav Fai <vladyslav.fai@accuristech.com>
2024-09-06 12:27:07 +0300

    setup application

--- server/server.js
+++ server/server.js
@@ -6,12 +6,18 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const appHost = express();
 const appMfe = express();
+const appMfe2 = express();
+const appMfe3 = express();
 
 const portHost = 4200; // host
 const portMfe = 4201;  //  mfe
+const portMfe2 = 4202;  //  mfe
+const portMfe3 = 4303;  //  mfe
 
-const mfeDirectory = path.join(__dirname, '../mfe1/dist/mfe1');
-const hostDirectory = path.join(__dirname, '../shell/dist/shell');
+const mfeDirectory = path.join(__dirname, '../mfe1/dist/mfe1/browser');
+const mfeDirectory2 = path.join(__dirname, '../mfe2/dist/mfe2/browser');
+const mfeDirectory3 = path.join(__dirname, '../mfe3/dist');
+const hostDirectory = path.join(__dirname, '../shell/dist/shell/browser');
 
 appHost.use(cors());
 
@@ -54,9 +60,21 @@ appHost.listen(portHost, () => {
 });
 
 appMfe.use(cors());
-
 appMfe.use(express.static(mfeDirectory));
-
 appMfe.listen(portMfe, () => {
     console.log(`MFE server is listening on http://localhost:${portMfe}`);
+});
+
+
+appMfe2.use(cors());
+appMfe2.use(express.static(mfeDirectory2));
+appMfe2.listen(portMfe2, () => {
+    console.log(`MFE 2 server is listening on http://localhost:${portMfe2}`);
+});
+
+
+appMfe3.use(cors());
+appMfe3.use(express.static(mfeDirectory3));
+appMfe3.listen(portMfe3, () => {
+    console.log(`MFE 3 server is listening on http://localhost:${portMfe3}`);
 });
